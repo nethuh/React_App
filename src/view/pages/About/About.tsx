d834a41b065e9576e29a9332c969c6be72225352
Sayani Nethmi <nethmihewage2002@gmail.com>
2023-12-22 22:35:43 +0530

    About updated

--- src/view/pages/About/About.tsx
+++ src/view/pages/About/About.tsx
@@ -1,14 +1,19 @@
 import {Component} from "react";
+import Logo from "../../../images/PigIcon.png"
 
 export class About extends Component {
     render() {
         return (
-            <>
-                <h1 className="text-4xl text-center
-                text-black">
-                    This is About Component
-                </h1>
-            </>
+            <div className="bg-pink-600 min-h-screen pt-40">
+                <div className="bg-pink-300 p-8 rounded-md shadow-md">
+                <h1 className="text-4xl font-bold mb-4 text-pink-700">About Us</h1>
+                    <div className="flex justify-between">
+                        <p>
+
+                        </p>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
