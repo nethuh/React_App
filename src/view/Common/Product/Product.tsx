97362a42a0a965f2e17aa4f1e9126a1651638680
Sayani Nethmi <nethmihewage2002@gmail.com>
2023-12-23 00:13:57 +0530

    Home updated

--- src/view/Common/Product/Product.tsx
+++ src/view/Common/Product/Product.tsx
@@ -1,4 +1,5 @@
 import {Component} from "react";
+import Img1 from "../../../images/pig.png";
 
 export class Product extends Component{
     render() {
--- src/view/pages/Home/Home.tsx
+++ src/view/pages/Home/Home.tsx
@@ -1,13 +1,42 @@
 import {Component} from "react";
+import { Product } from "../../Common/Product/Product";
+
+
 
 export class Home extends Component {
+    constructor(props: {} | Readonly<{}>) {
+        super(props);
+        this.state = {
+            data: [],
+        };
+    }
+
+    componentDidMount() {
+        this.fetchData();
+    }
+
+    fetchData = async () => {
+        try {
+            const response = await fetch("./product-data.json");
+            const jsonData = await response.json();
+            this.setState({data: jsonData});
+
+        }catch (error){
+            console.log("Error Fetching Data");
+        }
+    };
+
+
     render() {
+
+        const { data } = this.state;
+
         return (
-            <>
-                <h1 className="text-4xl text-center text-black">
-                    This is Home Component
-                </h1>
-            </>
+            <div className="flex justify-between pl-64 pr-64 pt-40 pb-44 bg-teal-200">
+                {data.map((product: any) => (
+                    <Product key={product.id} data={product} />
+                ))}
+            </div>
         );
     }
 }
