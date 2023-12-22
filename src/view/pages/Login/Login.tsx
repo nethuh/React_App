a55a40bc7214dc5c64420beb7212bd52e021f0b1
Sayani Nethmi <nethmihewage2002@gmail.com>
2023-12-22 23:59:15 +0530

    Login done

--- src/view/pages/Login/Login.tsx
+++ src/view/pages/Login/Login.tsx
@@ -9,11 +9,12 @@ export class Login extends Component {
                     <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                         <a href="#" className="flex items-center mb-4 text-5xl font-semibold text-pink-950">
                             <img className="w-28 h-28 mr-2"
-                            src={Logo}/>
+                                 src={Logo}/>
                             Piggy
                         </a>
 
-                        <div className="w-[45%] bg-white rounded-lg shadow dark:border   xl:p-0 dark:bg-pink-400 dark:border-gray-700">
+                        <div
+                            className="w-[45%] bg-white rounded-lg shadow dark:border   xl:p-0 dark:bg-pink-400 dark:border-gray-700">
                             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                                 <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-black">
                                     Sign in to your account
@@ -37,14 +38,37 @@ export class Login extends Component {
                                         />
                                     </div>
 
+                                    <div className="flex items-center justify-between">
+                                        <div className="flex items-start">
+                                            <div className="flex items-center h-5">
+                                                <input id="remember" aria-describedby="remember" type="checkbox"
+                                                       className="w-4 h-4 border border-gray-300 rounded bg-gray-600 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                                                />
+                                            </div>
+
+                                            <div className="ml-3 text-sm">
+                                                <label htmlFor="remember" className="text-gray-500 dark:text-gray-900">Remember
+                                                    me</label>
+                                            </div>
+                                        </div>
+                                        <a href="#"
+                                           className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot
+                                            password?</a>
+                                    </div>
+                                    <button type="submit"
+                                            className="w-full text-white bg-primary-600 hover:bg-pink-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-pink-800 dark:hover:bg-pink-900 dark:focus:ring-primary-800">Sign
+                                        in
+                                    </button>
+                                    <p className="text-sm font-light text-gray-500 dark:text-black">
+                                        Don’t have an account yet? <a href="#"
+                                                                      className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign
+                                        up</a>
+                                    </p>
                                 </form>
                             </div>
-
                         </div>
                     </div>
-
                 </section>
-
             </>
         );
     }
