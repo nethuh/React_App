2c7925e9fe7b850546c38f92a2165a2ff20f65b7
Sayani Nethmi <nethmihewage2002@gmail.com>
2023-12-22 22:28:26 +0530

    mainContent updated

--- src/view/footer/Footer.tsx
+++ src/view/footer/Footer.tsx
@@ -1,6 +1,4 @@
 import {Component} from "react";
-import logo from "../../images/PigIcon.png"
-
 
 export class Footer extends Component {
     render() {
--- src/view/mainContent/MainContent.tsx
+++ src/view/mainContent/MainContent.tsx
@@ -7,22 +7,14 @@ import {Contact} from "../pages/Contact/Contact";
 export class MainContent extends Component {
     render() {
         return (
-                <div className=" pt-80 pb-80 mx-auto bg-emerald-500">
-
-                    <Routes>
-                        <Route path="/" Component={Home}></Route>
-                        <Route path="/about" Component={About}></Route>
-                        <Route path="/contact" Component={Contact}></Route>
-                    </Routes>
-                    {/*<div className="h-20 w-20 text-center bg-black text-fuchsia-500*/}
-                    {/*rotate-45*/}
-                    {/*shadow-gray-900 shadow-xl hover:shadow-red-50*/}
-                    {/*cursor-pointer*/}
-                    {/*min-h-screen max-h-screen*/}
-                    {/*mx-auto">*/}
-                    {/*    <h1>Height & width</h1>*/}
-                    {/*</div>*/}
-                </div>
+            // <div className=" pt-80 pb-80 mx-auto bg-emerald-500">
+            <>
+                <Routes>
+                    <Route path="/" Component={Home}></Route>
+                    <Route path="/about" Component={About}></Route>
+                    <Route path="/contact" Component={Contact}></Route>
+                </Routes>
+            </>
         );
     }
 }
