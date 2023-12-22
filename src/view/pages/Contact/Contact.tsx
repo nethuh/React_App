d91b9ae5045f2e31c0be3f42f9a1cf4bef23ae3b
Sayani Nethmi <nethmihewage2002@gmail.com>
2023-12-22 22:44:56 +0530

    Contact updated

--- src/view/pages/Contact/Contact.tsx
+++ src/view/pages/Contact/Contact.tsx
@@ -3,11 +3,22 @@ import {Component} from "react";
 export class Contact extends Component {
     render() {
         return (
-            <>
-                <h1 className="text-4xl text-center text-black">
-                    This is Contact Component
-                </h1>
-            </>
+            <div className="bg-pink-600">
+                <section className="bg-white dark:bg-pink-600">
+                    <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+                        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
+                        <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-white sm:text-xl">Got
+                            a technical issue? Want to send feedback about a beta feature? Need details about our Business
+                            plan? Let us know.</p>
+                        <form action="#" className="space-y-8">
+                            <div>
+
+                            </div>
+                        </form>
+                    </div>
+                </section>
+
+            </div>
         );
     }
 }
