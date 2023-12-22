cacce5c611d6665a13ac989fccf4b9d742144b96
Sayani Nethmi <nethmihewage2002@gmail.com>
2023-12-22 23:55:04 +0530

    Login updated

--- src/view/pages/Login/Login.tsx
+++ src/view/pages/Login/Login.tsx
@@ -21,12 +21,20 @@ export class Login extends Component {
                                 <form className="space-y-4 md:space-y-6" action="#">
                                     <div>
                                         <label htmlFor="email"
-                                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">
+                                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">
                                             Your Email
                                         </label>
                                         <input type="email" name="email" id="email"
                                                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                                placeholder="name@company.com"/>
+                                    </div>
+
+                                    <div>
+                                        <label htmlFor="password"
+                                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Password</label>
+                                        <input type="password" name="password" id="password" placeholder="••••••••"
+                                               className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        />
                                     </div>
 
                                 </form>
