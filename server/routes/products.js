d70dee15022a208f7c718cd1ff438d90922c88d2
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-01-06 14:47:25 +0530

    updated

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,10 +1,13 @@
 var express = require('express');
+const ProductsController = require("../controller/ProductsController");
 var router = express.Router();
-const productsList = require('../constants/const')
+// const [productsList] = require('../constants/const')
+const {productsList} = require('../constants/const')
 
 /* GET all products listing. */
-router.get('/all', function(req, res, next) {
-  res.send(productsList);
-});
+// http://localhost:4000/products/all
+router.get('/all', ProductsController.GetAllProducts);
+
+router.post('/save', ProductsController.SaveProduct);
 
 module.exports = router;
